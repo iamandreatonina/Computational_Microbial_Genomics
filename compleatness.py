f614b945736d117975b022e4f0c0346a0f5092ef
iamandretuna <andrea.tonina97@gmail.com>
2023-03-31 17:15:27 +0200

    code for compleatness of the bins of the SGB

---
+++ compleatness.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+
+df = pd.read_table('SGB15132_bin_data.tsv',header= 0)
+
+df.plot(kind='bar', x= 'bin', y='completeness')
+plt.ylim(95,100)
+plt.legend()
+plt.title('completeness of MAGs')
+plt.show()
+plt.close()
+
+plt.savefig('completeness_distribution.png',dpi = 900)
