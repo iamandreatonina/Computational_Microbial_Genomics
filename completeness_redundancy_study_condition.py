a2bf53c91d5d8789d094a25dae591f8bbc7c479f
iamandretuna <andrea.tonina97@gmail.com>
2023-04-04 19:03:21 +0200

    other changes

--- completeness_redundancy_study_condition.py
+++ completeness_redundancy_study_condition.py
@@ -2,8 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 
 
-df = pd.read_table('SGB15132_bin_data.tsv',header= 0)
-df2 = pd.read_table('SGB15132_metadata.tsv',header= 0)
+df = pd.read_table('../SGB15132_bin_data.tsv',header= 0)
+df2 = pd.read_table('../SGB15132_metadata.tsv',header= 0)
 
 ####### completeness
 min_completeness = df['completeness'].min()
@@ -12,13 +12,13 @@ max_completeness = df['completeness'].max()
 print('{} \t {} \t {}'.format('min','avg','max'))
 print('{} \t {} \t {}'.format(min_completeness,avg_completeness,max_completeness))
 
-df.plot(kind='bar', x= 'bin', y='completeness')
-plt.ylim(95,100)
-plt.legend()
-plt.title('completeness of MAGs')
-plt.savefig('completeness_distribution.png')
-plt.show()
-plt.close()
+# df.plot(kind='bar', x= 'bin', y='completeness')
+# plt.ylim(95,100)
+# plt.legend()
+# plt.title('completeness of MAGs')
+# plt.savefig('completeness_distribution.png')
+# plt.show()
+# plt.close()
  
 ############ redundancy
 min_redundancy = df['redundancy'].min()
@@ -37,8 +37,8 @@ mylabels = ['control', 'hypertension','AS','adenoma','pre-hypertension','cirrhos
 myexplode = [0.2, 0, 0, 0, 0, 0]
 
 plt.pie(stud_cond,labels= mylabels, explode = myexplode, shadow = True)
-plt.show()
 plt.savefig('Study_condition.png')
+plt.show()
 plt.close()
 
 
